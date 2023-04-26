e7f6cd97fcbbf42356ef5d5069f50850fca38a47
Justin <ohmlotusphoenix@gmail.com>
2023-04-26 12:00:49 -0500

    updated nuget packages: and added dbcontext file and updated program.cs file

---
+++ Moonwalkers/Data/InventoryDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Build.Tasks.Deployment.Bootstrapper;
+using Microsoft.EntityFrameworkCore;
+using Moonwalkers.Models;
+
+namespace Moonwalkers.Data
+{
+    public class InventoryDbContext : DbContext
+    {
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options)
+            : base(options)
+        {
+        }
+
+
+    }
+}
+
--- Moonwalkers/Program.cs
+++ Moonwalkers/Program.cs
@@ -1,7 +1,36 @@
+using Moonwalkers.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 10;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = false;
+})
+.AddEntityFrameworkStores<InventoryDbContext>(); // change to InventoryDbContext here
+
+//--- MySql connection
+
+//pomelo connection syntax: https://github.com/PomeloFoundation/Pomelo.EntityFrameworkCore.MySql
+// working with the .NET 6 (specifically the lack of a Startup.cs)
+//https://learn.microsoft.com/en-us/aspnet/core/migration/50-to-60-samples?view=aspnetcore-6.0#add-configuration-providers
+
+var connectionString = "server=localhost;user=inventorymanagement;password=inventorymanagement;database=inventorymanagement";
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
+
+builder.Services.AddDbContext<InventoryDbContext>(dbContextOptions => dbContextOptions.UseMySql(connectionString, serverVersion));
+
+//--- end of connection syntax
 
 var app = builder.Build();
 
@@ -9,7 +38,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -17,8 +45,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
+app.MapRazorPages();
+app.MapControllers();
 
 app.MapControllerRoute(
     name: "default",
